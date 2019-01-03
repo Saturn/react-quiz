bc21003b2daaf5bc263e908b016907bdd98577e3
Patrick McMichael <patrick@patrickmcmichael.org>
2019-01-03 16:33:34 +0000

    Change Questions component to Quiz

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Questions from './Components/Questions';
+import Quiz from './Components/Quiz';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Questions />
+        <Quiz />
       </div>
     );
   }
--- src/Components/Quiz.js
+++ src/Components/Quiz.js
@@ -1,13 +1,44 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchQuestions, fetchQuestion } from '../store/actions';
 
+import Question from './Question';
 
 
 class Quiz extends Component {
-  render() {
-    return (
 
-    );
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    const { dispatch } = this.props;
+    dispatch(fetchQuestions());
+  }
+
+  render() {
+      if (!this.props.isLoading) {
+        return (
+          <div>
+        {this.props.questions.map((item, i) => {
+            return <Question question={item.question} answers={item.answers} />
+        })}
+        </div>
+        );
+      }
+      else {
+        return (
+          <p>Loading...</p>
+          );
+      }
   }
 }
 
-export default Quiz;
+const mapStateToProps = state => {
+  return {
+    questions: state.questions,
+    isLoading: state.isLoading
+  }
+};
+
+export default connect(mapStateToProps)(Quiz);
