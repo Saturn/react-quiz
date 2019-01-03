901bb05afeeddcc7e4768585d1f5256083341d16
Patrick McMichael <patrick@patrickmcmichael.org>
2019-01-03 16:36:41 +0000

    Use new Answer component

--- src/Components/Question.js
+++ src/Components/Question.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchQuestions, fetchQuestion } from '../store/actions';
+import Answer from './Answer';
 
 class Question extends Component {
 
@@ -13,10 +14,11 @@ class Question extends Component {
       <div>
         <p dangerouslySetInnerHTML={{__html: this.props.question}}></p>
         <ul>
-        {this.props.answers.map(item => {
-          return <li>{item}</li>
-        })
-      }
+        {
+          this.props.answers.map(item => {
+            return <Answer answer={item} />
+          })
+        }
         </ul>
       </div>
       );
