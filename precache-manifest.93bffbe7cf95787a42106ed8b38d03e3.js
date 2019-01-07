3826c7f9066321d83f0fce8f1eb85be57aa24c6b
Patrick McMichael <patrick@patrickmcmichael.org>
2019-01-07 16:56:40 +0000

    Updates

---
+++ precache-manifest.93bffbe7cf95787a42106ed8b38d03e3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "af45168ecde8330867c4",
+    "url": "/react-quiz/static/js/runtime~main.af45168e.js"
+  },
+  {
+    "revision": "576c3dbb3a6320348ccf",
+    "url": "/react-quiz/static/js/main.576c3dbb.chunk.js"
+  },
+  {
+    "revision": "eca472d6028635a7938a",
+    "url": "/react-quiz/static/js/1.eca472d6.chunk.js"
+  },
+  {
+    "revision": "576c3dbb3a6320348ccf",
+    "url": "/react-quiz/static/css/main.a0a16a17.chunk.css"
+  },
+  {
+    "revision": "9c5d85379589a86bc0735ac8c9d79a3a",
+    "url": "/react-quiz/index.html"
+  }
+];
